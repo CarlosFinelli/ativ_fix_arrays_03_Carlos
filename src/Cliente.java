034872e51476d346ade4460c0c1b558ff72c7158
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2021-04-06 17:14:20 -0300

    Conclusão da atividade proposta

---
+++ src/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente extends Pessoa{
+    private String cpf;
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/ClienteEspecial.java
@@ -0,0 +1,11 @@
+public class ClienteEspecial extends Pessoa{
+    private String cnpj;
+
+    public String getCnpj() {
+        return this.cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
